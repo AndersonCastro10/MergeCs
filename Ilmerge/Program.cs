8df4f02bdd476c942845753cebe3668e9eea6e13
AndersonCastro <anderson-lima-10@hotmail.com>
2020-04-17 15:07:59 -0300

    Programa console simples. E necessario colocar o caminho onde estão os arquivos. Irá gerar uma string com o comando ilimerge, depois basta abrir o cmd onde está os arquivos colar o comando e rodar.

--- Ilmerge/Program.cs
+++ Ilmerge/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,31 @@ namespace Ilmerge
     {
         static void Main(string[] args)
         {
+            string caminho = @"C:\Users\ander\source\repos\cristianohij\IntegrosCS\packages\ILMerge.3.0.29\tools\net452";
+            string ignorar = "System.Compiler.dll";
+            // tenho que tirar o System.Compiler.dll , faz parte do Ilmerge
+            FileInfo arquivo;
+            StringBuilder str = new StringBuilder();
+            str.Append("ilmerge.exe /target:winexe /allowDup /out:relatoriointegros.exe RelatorioReports.exe ");
+
+            // Ler todos os arquivos de um determinado caminho
+
+            string[] arquivos = Directory.GetFiles(caminho, "*.dll");
+
+            foreach (var item in arquivos)
+            {
+                arquivo = new FileInfo(item);
+                Console.WriteLine(arquivo.Name);
+
+                if (arquivo.Name != ignorar)
+                {
+                    str.Append(arquivo.Name + " "); 
+                }
+            }
+
+            Console.WriteLine(str);
+
+            Console.ReadLine();
         }
     }
 }
